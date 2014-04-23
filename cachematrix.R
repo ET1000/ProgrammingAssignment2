db4bb8371a9398061ef3ad4e875f9228a8fc5bd7
Eug <eut@hotmail.com>
2014-04-23 14:34:49 -0700

    updated the Assignement 2
    the two functions have  been written

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-##This is a test file for seeing how the system works to update the file in the repo
+## this function will take a matrix and return a list of useful functions:
+## get() to extract/read/show the matrix
+## set() directly to change the underlying matrix
+## getinv() to get the inverse of the matrix
+## setinv() to store the value of the matrix inverse for future use
+## Note: that the function set() resets the cached mean every time its called with 
+## a new vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## this is the function that will actually solve the inversion using the previous function if needed
+## starts by checking is there is already an inverse (from a call to $getinv) and 
+## if not found then proceeds to calulate the inverse 
+## after calylating the inverse the function x$setinv() is called that will store it
+## for later use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
